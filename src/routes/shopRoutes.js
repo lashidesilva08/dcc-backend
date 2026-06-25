1592130a6aa90d72fca2da8a2f7ec7fab404eece
Tharindi2004 <tharindi.20240581@iit.ac.lk>
2026-06-25 12:35:39 +0530

    feat: reorganize shop routes for improved readability and maintainability

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -6,16 +6,24 @@ import {
   updateShop,
   getShopAnalytics,
   getAllShops,
-  getShopByUrl
+  getShopByUrl,
+  getShopProductsBySlug,
 } from "../controllers/shopControllers.js";
 
 const router = express.Router();
 
 router.get("/", getAllShops);
+
 router.post("/", createShop);
-router.get("/:id", getShopById);
-router.put("/:id", updateShop);
-router.get("/my-shop/analytics", getShopAnalytics);
+
 router.get("/url/:shopUrl", getShopByUrl);
+
+router.get("/url/:shopUrl/products", getShopProductsBySlug);
+
+router.get("/:id", getShopById);
+
+router.put("/:id", updateShop);
+
+router.get("/my-shop/analytics", getShopAnalytics);
 
 export default router;
